6fb135eef4e97bbde2ad7a495e86e6aaf7bc3459
oscar Info <racso1607@gmail.com>
2024-12-27 14:43:59 -0500

    add create_transaction and get_transaction

--- src/services/transaction/transaction.service.ts
+++ src/services/transaction/transaction.service.ts
@@ -8,22 +8,17 @@ import * as crypto from 'crypto';
 @Injectable()
 export class TransactionService {
   private readonly api_url: string;
-  private readonly api_key: string;
+  private readonly api_public_key: string;
+  private readonly api_private_key: string;
   private readonly integrity_key: string;
   constructor(
     private readonly httpService: HttpService,
     private readonly configService: ConfigService,
   ) {
     this.api_url = this.configService.get<string>('WOMPI_API_URL');
-    this.api_key = this.configService.get<string>('WOMPI_PUBLIC_KEY');
+    this.api_public_key = this.configService.get<string>('WOMPI_PUBLIC_KEY');
+    this.api_private_key = this.configService.get<string>('WOMPI_PRIVATE_KEY');
     this.integrity_key = this.configService.get<string>('WOMPI_INTEGRITY_KEY');
-  }
-
-  async get_acceptance_tokens(): Promise<string> {
-    const response: AxiosResponse = await firstValueFrom(
-      this.httpService.get(`${this.api_url}merchants/${this.api_key}`),
-    );
-    return response.data.data.id;
   }
 
   //<Referencia><Monto><Moneda><SecretoIntegridad>"
@@ -60,5 +55,71 @@ export class TransactionService {
       }),
     );
     return response.data.data.id;
+  }
+
+  async get_acceptance_tokens(): Promise<string> {
+    const response: AxiosResponse = await firstValueFrom(
+      this.httpService.get(`${this.api_url}merchants/${this.api_public_key}`),
+    );
+    return response.data.data.id;
+  }
+  async create_transaction_with_transaction({
+    acceptance_token,
+    amount_in_cents,
+    currency,
+    transaction_reference,
+    customer_email,
+    credit_card_token,
+    installments,
+  }: {
+    acceptance_token: string;
+    amount_in_cents: number;
+    currency: string;
+    customer_email: string;
+    transaction_reference: string;
+    credit_card_token: string;
+    installments: number;
+  }): Promise<string> {
+    const signature = await this.generate_signature({
+      transaction_reference,
+      amount_in_cents,
+      currency,
+    });
+    const response: AxiosResponse = await firstValueFrom(
+      this.httpService.post(
+        `${this.api_url}transactions`,
+        {
+          acceptance_token,
+          amount_in_cents,
+          currency,
+          customer_email,
+          reference: transaction_reference,
+          signature,
+          payment_method: {
+            type: 'CARD',
+            token: credit_card_token,
+            installments: installments,
+          },
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${this.api_public_key}`,
+          },
+        },
+      ),
+    );
+    return response.data;
+  }
+  async get_transaction(transaction_id: string): Promise<string> {
+    const response: AxiosResponse = await firstValueFrom(
+      this.httpService.get(`${this.api_url}transactions/${transaction_id}`, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${this.api_public_key}`,
+        },
+      }),
+    );
+    return response.data;
   }
 }
